712fa9e2e503fe9918a4b735c0031b5e84d4c010
Aymen Kadri <82622888+aymendev8@users.noreply.github.com>
2024-03-11 22:12:29 +0000

    Mise en page de la page de connexion et ajout du fichier users.json

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,11 +1,82 @@
 <template>
-  <p>dfhgdiufhgiusdhrfkhgfdskljgbioudfopuhqergvhqdiufhbsiu</p>
+  <div class="container">
+    <h3 class="title">Se connecter | rhTool</h3>
+    <q-input
+      v-model="email"
+      filled
+      type="email"
+      hint="Email"
+      class="input"
+      color=""
+    />
+    <q-input
+      v-model="password"
+      filled
+      :type="isPwd ? 'password' : 'text'"
+      hint="Password"
+      class="input"
+    >
+      <template v-slot:append>
+        <q-icon
+          :name="isPwd ? 'visibility_off' : 'visibility'"
+          class="cursor-pointer"
+          @click="togglePasswordVisibility"
+        />
+      </template>
+    </q-input>
+    <q-btn color="black" label="connexion" @click="login" />
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-  name: 'LoginPage'
-})
+  name: "LoginPage",
+  setup() {
+    const email = ref("");
+    const password = ref("");
+    const isPwd = ref(true);
+
+    const togglePasswordVisibility = () => {
+      isPwd.value = !isPwd.value;
+    };
+
+    const login = () => {
+      console.log(email.value, password.value);
+    };
+
+    return {
+      email,
+      password,
+      isPwd,
+      togglePasswordVisibility,
+      login,
+    };
+  },
+});
 </script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: auto;
+  width: 50%;
+  height: 50vh;
+}
+
+.input {
+  width: 300px;
+  padding-bottom: 2rem;
+}
+.title {
+  margin-bottom: 2rem;
+  font-size: 2rem;
+  font-weight: bold;
+  color: #1976d2;
+  text-align: center;
+}
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,32 +1,17 @@
-import { AdminRoles } from "src/const/adminRoles";
-
 const routes = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
-    children: [
-      { path: "", component: () => import("pages/IndexPage.vue") },
-      {
-        path: "/users",
-        component: () => import("pages/UsersPage.vue"),
-        meta: { roles: [AdminRoles.RH] },
-      },
-    ],
-    meta: { requiresAuth: true },
+    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
   },
   {
     path: "/login",
-    component: () => import("layouts/AuthLayout.vue"),
-    children: [{ path: "", component: () => import("pages/LoginPage.vue") }],
-    meta: { requiresAuth: false },
+    component: () => import("pages/LoginPage.vue"),
   },
   {
-    path: "/unauthorized",
+    path: "/users",
     component: () => import("layouts/MainLayout.vue"),
-    children: [
-      { path: "", component: () => import("pages/UnauthorizedPage.vue") },
-    ],
-    meta: { requiresAuth: false },
+    children: [{ path: "", component: () => import("pages/UsersPage.vue") }],
   },
   // Always leave this as last one,
   // but you can also remove it
